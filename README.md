5925d322e93334187ceff94c134e7d122363ce1e
nabil <merchant.nabil@gmail.com>
2022-01-29 11:41:26 -0600

    creating a small page that guesses a users name and age using some APIs.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 ## Demonstrating jinja
 This small program is to demonstrate the usage of Flask and the jinja templating language. 
+The user can enter their name in the url and the site will return their predicted age and gender. 
+Age and gender predictions are derived using the [Agify](https://agify.io/) and [Genderize](https://genderize.io/) APIs.
+
+Example:
+```
+http://127.0.0.1:5000/guess/nabil
+```
+will return a predicted age and gender of the name `nabil`. 
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import random
 import datetime
+import requests
 
 app = Flask(__name__)
 
@@ -10,6 +11,15 @@ def home():
     random_number = random.randint(1, 10)
     current_year = datetime.datetime.now().year
     return render_template("index.html", num=random_number, year=current_year)
+
+
+@app.route('/guess/<string:name>')
+def predict_age_and_gender(name):
+    age_response = requests.get(url="https://api.agify.io/?name=" + name)
+    age = age_response.json()["age"]
+    gender_response = requests.get(url="https://api.genderize.io?name=" + name)
+    gender = gender_response.json()["gender"]
+    return render_template("answer.html", name=name.capitalize(), age=age, gender=gender)
 
 
 if __name__ == '__main__':
